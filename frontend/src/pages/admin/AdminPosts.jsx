5e458f6ab11cf1d19e879048a26bc6161ee679ed
joonsol <71648638+joonsol@users.noreply.github.com>
2025-11-06 17:25:36 +0900

    update

--- frontend/src/pages/admin/AdminPosts.jsx
+++ frontend/src/pages/admin/AdminPosts.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-
+import React ,{useEffect,useState}from 'react'
+import {fetchAdminPosts, patchAdminPost} from '../../api/adminApi'
+import AdminFilter from '../../components/admin/AdminFilter'
+import AdminPostsList from '../../components/admin/AdminPostsList'
 const AdminPosts = () => {
+  const [list, setList]=useState([])
+
+  const [query, setQuery]=useState({
+    page:1,
+    size:10,
+    status:'',
+    q:'',
+    user:''
+  })
+
+
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const items=await fetchAdminPosts(query)
+        setList(items)
+      } catch (error) {
+        console.error('게시글 불러오기 실패', error)
+      }
+    })
+  },[query])
   return (
-    <div>ADminPosts</div>
+    <div>
+      <AdminFilter/>
+      <AdminPostsList items={list}/>
+    </div>
   )
 }
 
